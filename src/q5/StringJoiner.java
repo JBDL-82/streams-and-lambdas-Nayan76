7a8838172da4edb73c8c0bb2d9674c43d9ec9e28
Nayan76 <skn.760210@gmail.com>
2025-03-29 13:01:20 +0530

    Updated StringJoiner.java in q5

--- src/q5/StringJoiner.java
+++ src/q5/StringJoiner.java
@@ -2,6 +2,7 @@ package q5;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StringJoiner {
     public static String joinWithComma(List<String> strings) {
@@ -11,14 +12,13 @@ public class StringJoiner {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        List<String> strings = new ArrayList<>();
 
         System.out.println("Enter strings to join (type 'done' when finished):");
-        while (true) {
-            String input = sc.nextLine();
-            if (input.equalsIgnoreCase("done")) break;
-            strings.add(input);
-        }
+
+        // Using Stream.generate() to create an infinite stream of inputs
+        List<String> strings = Stream.generate(sc::nextLine)
+                .takeWhile(input -> !input.equalsIgnoreCase("done"))
+                .collect(Collectors.toList());
 
         String result = joinWithComma(strings);
         System.out.println("\nJoined result:");
